1d5749affdd80d34d3eb00e9e0e7111c9d74b0af
Andrey Panov <rockford@yandex.ru>
2018-02-19 21:38:58 +0300

    mt7612u: Update timer setup code for kernels >= 4.15
    Signed-off-by: Andrey Panov <rockford@yandex.ru>

--- include/os/rt_linux.h
+++ include/os/rt_linux.h
@@ -374,7 +374,11 @@ typedef struct tasklet_struct  *POS_NET_TASK_STRUCT;
 typedef struct timer_list	OS_NDIS_MINIPORT_TIMER;
 typedef struct timer_list	OS_TIMER;
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,15,0))
+typedef void (*TIMER_FUNCTION)(struct timer_list *);
+#else
 typedef void (*TIMER_FUNCTION)(unsigned long);
+#endif
 
 #define RTMP_TIME_AFTER(a,b)		\
 	(typecheck(unsigned long, (unsigned long)a) && \
